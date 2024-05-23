df3692cc25536b9e076737f3970028d02b310f82
Anton <itonyemail@gmail.com>
2024-05-22 20:11:30 -0700

    initial setup

---
+++ Assets/Scripts/Water.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class Water : MonoBehaviour
+{
+    public Shader waterShader;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
