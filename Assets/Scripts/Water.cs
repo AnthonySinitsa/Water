38529f4b1388a3ffdbf5aa3fa6ec5aa46bc1588c
Anton <itonyemail@gmail.com>
2024-05-22 21:26:11 -0700

    basic wave achieved

--- Assets/Scripts/Water.cs
+++ Assets/Scripts/Water.cs
@@ -6,16 +6,87 @@ using UnityEngine;
 public class Water : MonoBehaviour
 {
     public Shader waterShader;
-    
-    // Start is called before the first frame update
+    public int size = 10;
+    public float waveSpeed = 0.2f;
+    public float waveScale = 0.5f;
+
+    private MeshFilter meshFilter;
+    private MeshRenderer meshRenderer;
+    private Mesh mesh;
+
     void Start()
     {
-        
+        meshFilter = GetComponent<MeshFilter>();
+        meshRenderer = GetComponent<MeshRenderer>();
+
+        mesh = new Mesh();
+        mesh.name = "Water Mesh";
+        mesh.vertices = GenerateVertices();
+        mesh.triangles = GenerateTriangles();
+        mesh.RecalculateNormals();
+
+        meshFilter.mesh = mesh;
+
+        if (waterShader)
+        {
+            meshRenderer.material = new Material(waterShader);
+        }
     }
 
-    // Update is called once per frame
+    Vector3[] GenerateVertices()
+    {
+        Vector3[] vertices = new Vector3[(size + 1) * (size + 1)];
+        for (int y = 0; y <= size; y++)
+        {
+            for (int x = 0; x <= size; x++)
+            {
+                vertices[y * (size + 1) + x] = new Vector3(x, 0, y);
+            }
+        }
+        return vertices;
+    }
+
+    int[] GenerateTriangles()
+    {
+        int[] triangles = new int[size * size * 6];
+        int index = 0;
+        for (int y = 0; y < size; y++)
+        {
+            for (int x = 0; x < size; x++)
+            {
+                triangles[index++] = (y * (size + 1)) + x;
+                triangles[index++] = ((y + 1) * (size + 1)) + x;
+                triangles[index++] = (y * (size + 1)) + x + 1;
+
+                triangles[index++] = ((y + 1) * (size + 1)) + x;
+                triangles[index++] = ((y + 1) * (size + 1)) + x + 1;
+                triangles[index++] = (y * (size + 1)) + x + 1;
+            }
+        }
+        return triangles;
+    }
+
     void Update()
     {
-        
+        Vector3[] vertices = mesh.vertices;
+        float time = Time.time * waveSpeed;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Vector3 vertex = vertices[i];
+            vertex.y = SumOfSines(vertex.x * waveScale, vertex.z * waveScale, time);
+            vertices[i] = vertex;
+        }
+
+        mesh.vertices = vertices;
+        mesh.RecalculateNormals();
     }
+
+    float SumOfSines(float x, float z, float t)
+    {
+        return Mathf.Sin(x + t * 0.75f) * 0.2f + 
+            Mathf.Sin(z + t * 0.5f) * 0.1f + 
+            Mathf.Sin((x + z) * 0.5f + t) * 0.05f;
+    }
+
 }
--- Assets/Shaders/WaterShader.shader
+++ Assets/Shaders/WaterShader.shader
@@ -2,62 +2,29 @@ Shader "Custom/WaterShader"
 {
     Properties
     {
-        _MainTex ("Base (RGB)", 2D) = "white" {}
-        _WaveSpeed ("Wave Speed", Float) = 0.2
-        _WaveScale ("Wave Scale", Float) = 0.5
+        _Color ("Color", Color) = (0.2, 0.5, 0.7, 1.0)
     }
     SubShader
     {
         Tags { "RenderType"="Opaque" }
-        LOD 100
-        Pass
+        LOD 200
+
+        CGPROGRAM
+        #pragma surface surf Lambert
+
+        struct Input
         {
-            CGPROGRAM
-            #pragma vertex vert
-            #pragma fragment frag
+            float3 worldPos;
+        };
 
-            #include "UnityCG.cginc"
+        float4 _Color;
 
-            struct appdata_t
-            {
-                float4 vertex : POSITION;
-                float2 uv : TEXCOORD0;
-            };
-
-            struct v2f
-            {
-                float2 uv : TEXCOORD0;
-                float4 vertex : SV_POSITION;
-            };
-
-            sampler2D _MainTex;
-            float _WaveSpeed;
-            float _WaveScale;
-
-            v2f vert (appdata_t v)
-            {
-                v2f o;
-                o.vertex = UnityObjectToClipPos(v.vertex);
-                o.uv = v.uv;
-                return o;
-            }
-
-            float sumOfSines(float x, float z, float t)
-            {
-                return sin(x * 1.0 + t * 0.75) * 0.2 + 
-                       sin(z * 1.5 + t * 0.5) * 0.1 + 
-                       sin((x + z) * 0.5 + t) * 0.05;
-            }
-
-            fixed4 frag (v2f i) : SV_Target
-            {
-                float t = _Time.y * _WaveSpeed;
-                float waveHeight = sumOfSines(i.uv.x * _WaveScale, i.uv.y * _WaveScale, t);
-                fixed4 col = tex2D(_MainTex, i.uv + waveHeight);
-                return col;
-            }
-            ENDCG
+        void surf(Input IN, inout SurfaceOutput o)
+        {
+            o.Albedo = _Color.rgb;
+            o.Alpha = _Color.a;
         }
+        ENDCG
     }
     FallBack "Diffuse"
 }
